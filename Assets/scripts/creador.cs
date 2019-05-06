8ed0c035b36ed7b7cf1637a828078b2b488748b4
JosueVA9 <a01339544@itesm.mx>
2019-05-05 23:04:43 -0500

    marciano intento de subir 2

---
+++ Assets/scripts/creador.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class creador : MonoBehaviour
+{
+    public float maxTime = 1;
+    public float timer = 0; 
+    public GameObject meteorito;
+    public float height;
+    
+
+        // Start is called before the first frame update
+    void Start()
+    {
+        GameObject newmeteorito = Instantiate(meteorito);
+        newmeteorito.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > maxTime)
+        {
+           GameObject newmeteorito = Instantiate(meteorito);
+            newmeteorito.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
+            Destroy(newmeteorito, 15);
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+    }
+}
